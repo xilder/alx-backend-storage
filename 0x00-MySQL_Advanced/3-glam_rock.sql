cf010b58244ce8d60cf59667fd2293e2fe95ff0d
xilder <olabowaleayobami@gmail.com>
2024-06-14 21:56:49 +0100

    adds task 3

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- lists all bands with Glam rock as their main style, ranked by their longevity
+
+SELECT band_name, (IFNULL(split, '2022') - formed) as lifespan
+FROM metal_bands
+WHERE FIND_IN_SET('Glam rock', style) > 0
+ORDER BY lifespan DESC;
