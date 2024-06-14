f1d1b485b5900d1fef8f9c0865ae5ac4f50380e2
xilder <olabowaleayobami@gmail.com>
2024-06-14 19:44:28 +0100

    adds task 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -3,7 +3,7 @@
 -- email - string (255 characters), never null and unique
 -- name - string (255 characters)
 
-CREATE TABLE IF NOT EXISTS user(
+CREATE TABLE IF NOT EXISTS users(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
