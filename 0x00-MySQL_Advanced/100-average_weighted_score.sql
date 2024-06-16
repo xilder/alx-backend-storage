417ba0a40f1b2db117cc559cdfa7ebbcb5a4c650
xilder <olabowaleayobami@gmail.com>
2024-06-16 22:19:55 +0100

    adds ask 100

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -4,21 +4,29 @@ DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
 DECLARE weighted_total INT;
-DECLARE result INT;
-SELECT SUM(projects.weight)
+DECLARE averager INT;
+SELECT SUM(projects.weight * corrections.score)
     INTO weighted_total
     FROM corrections
     INNER JOIN projects
     ON corrections.project_id = projects.id
     WHERE corrections.user_id = user_id;
-SELECT SUM(projects.weight * corrections.score / weighted_total)
-    INTO result
+
+SELECT SUM(projects.weight)
+    INTO averager
     FROM corrections
     INNER JOIN projects
     ON corrections.project_id = projects.id
     WHERE corrections.user_id = user_id;
-UPDATE users
-    SET average_score = result
-    WHERE id = user_id;
+
+IF averager = 0 THEN
+    UPDATE users
+    SET average_score = 0
+    WHERE users.id = user_id;
+ELSE
+    UPDATE users
+        SET average_score = weighted_total / averager
+        WHERE users.id = user_id;
+END IF;
 END $$
 DELIMITER ;
