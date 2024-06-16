5edac1f88b17f70af8f0cf6b61043bcac7c6c9de
xilder <olabowaleayobami@gmail.com>
2024-06-16 14:56:55 +0100

    adds task 5

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,15 @@
+-- trigger that resets the attribute valid_email only when the email has been changed
+
+DROP TRIGGER IF EXISTS mail_validation_reset;
+DELIMITER $$
+CREATE TRIGGER mail_validation_reset
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email
+    THEN
+    SET NEW.valid_email = 0;
+    END IF;
+END $$
+DELIMITER ;
+
