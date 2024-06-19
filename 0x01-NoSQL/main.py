f2ce06f7af7edd0ec5b330bcd3a62e211e6e4409
xilder <olabowaleayobami@gmail.com>
2024-06-19 01:43:56 +0100

    adds task 10

--- 0x01-NoSQL/main.py
+++ 0x01-NoSQL/main.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
-""" 9-main """
+""" 10-main """
 from pymongo import MongoClient
 list_all = __import__('8-all').list_all
-insert_school = __import__('9-insert_school').insert_school
+update_topics = __import__('10-update_topics').update_topics
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     school_collection = client.my_db.school
-    new_school_id = insert_school(school_collection, name="UCSF", address="505 Parnassus Ave")
-    print("New school created: {}".format(new_school_id))
+    update_topics(school_collection, "Holberton school", ["Sys admin", "AI", "Algorithm"])
 
     schools = list_all(school_collection)
     for school in schools:
-        print(school)
+        print("[{}] {} {}".format(school.get('_id'), school.get('name'), school.get('topics', "")))
+
+    update_topics(school_collection, "Holberton school", ["iOS"])
+
+    schools = list_all(school_collection)
+    for school in schools:
+        print("[{}] {} {}".format(school.get('_id'), school.get('name'), school.get('topics', "")))
