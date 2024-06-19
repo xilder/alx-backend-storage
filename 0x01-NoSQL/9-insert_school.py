032df248eda2cce2d717e89824cdbf1b9bacf8a3
xilder <olabowaleayobami@gmail.com>
2024-06-19 01:33:53 +0100

    adds task 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,8 @@
+#!/usr/bin env python3
+"""inserts a new document in a collection based on kwargs"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """inserts a new document in a collection based on kwargs"""
+    new_doc = mongo_collection.insert_one(kwargs)
+    return new_doc.inserted_id
