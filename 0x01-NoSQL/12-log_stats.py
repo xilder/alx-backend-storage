cb642a7c56b2a5fe5798a9a63079d5ae4eb9118b
xilder <olabowaleayobami@gmail.com>
2024-06-20 17:20:35 +0100

    updates task 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,38 +7,22 @@ logs stored in MongoDB
 from pymongo import MongoClient
 
 
-def log_stats():
+def log_stats(mongo_collection):
     """
     provides some stats about Nginx
     logs stored in MongoDB
     """
-    client = MongoClient('mongodb://localhost:27017')
-    methods = {
-        'GET': 0,
-        'POST': 0,
-        'PUT': 0,
-        'PATCH': 0,
-        'DELETE': 0,
-    }
-    status = 0
-    nginx = client.logs.nginx
-    all_objs = nginx.find()
-    count = 0
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    status = mongo_collection.count_documents({})
+    print(f"{status} logs")
+    for method in methods:
+        method_len = len(list(mongo_collection.find({"method": method})))
+        print(f"\tmethod {method}: {method_len}")
+    count = mongo_collection.count_documents({"method": "GET"})
 
-    for obj in all_objs:
-        count += 1
-        if obj['method'] in methods.keys():
-            methods[obj['method']] += 1
-
-        if obj['method'] == 'GET' and obj['path'] == "/status":
-            status += 1
-
-    print(f"{count} logs")
-    print("Methods:")
-    for k in methods.keys():
-        print(f"    method {k}: {methods[k]}")
-    print(f"{status} status check")
+    print(f"{count} status check")
 
 
 if __name__ == "__main__":
-    log_stats()
+    client = MongoClient('mongodb://localhost:27017')
+    log_stats(client.logs.nginx)
