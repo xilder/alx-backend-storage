a2eb2552c35e90fb75bca954f785710f008c2633
xilder <olabowaleayobami@gmail.com>
2024-06-23 18:18:57 +0100

    adds task 5

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""requests an html page and returns the number of times it was called"""
+import requests
+import redis
+from functools import wraps
+from typing import Callable, Any
+
+rd = redis.Redis()
+
+
+def data_cacher(method: Callable) -> Any:
+    """caches the number of times a function a site called"""
+
+    @wraps(method)
+    def cacher(url: str) -> str:
+        """cache wrapper"""
+        rd.incr(f"count:{url}")
+        result = rd.get(f"result:{url}")
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        rd.setex(f"result:{url}", 10, result)
+        return result
+
+    return cacher
+
+
+@data_cacher
+def get_page(url: str) -> str:
+    """get_page function"""
+    res = requests.get(url).text
+    return res
